7d71e8280b1bd837c61879192813bf0f08698281
jashoney <4479@holbertonschool.com>
2022-04-26 12:59:46 +1000

    added task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks for a loop in a linked list
+ * @list: a ptr to the list head
+ * Return: 0 with no loop, 1 with a loop
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list, *slow = list;
+	int i = 0;
+
+	while (fast != NULL)
+	{
+		fast = fast->next;
+		if (fast->next == NULL)
+			return (0);
+		slow = slow->next;
+		fast = fast->next;
+		if (fast == slow)
+			return (1);
+		i++;
+		if (i > 20)
+			return (0);
+	}
+	return (0);
+}
