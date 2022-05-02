6d3b005145f637466222860699534dbbd2c6c6ec
jashoney <4479@holbertonschool.com>
2022-05-02 12:24:31 +1000

    changed task 3 to print individually

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,8 @@
 
 def print_reversed_list_integer(my_list=[]):
     new_list = []
-    length = len(my_list) - 1
-    for i in range(length + 1):
-        new_list.append(my_list[length - i])
-    print(new_list)
+    length = len(my_list)
+    for i in range(length):
+        new_list.append(my_list[length - 1 - i])
+    for i in range(length):
+        print(new_list[i])
