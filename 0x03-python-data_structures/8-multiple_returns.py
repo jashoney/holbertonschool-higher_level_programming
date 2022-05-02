cdb7b9686a7b5d01bbfe502e9569a3b3068f3cc6
jashoney <4479@holbertonschool.com>
2022-05-02 12:39:47 +1000

    added NULL test

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
-    first_char = sentence[0]
-    new_t = (length, first_char)
-    return (new_t)
+    if len(sentence) > 0:
+        return (len(sentence), sentence[0])
+    else:
+        return (0, None)
