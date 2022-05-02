72eff0d35d1d1c919759f4567b778900b8c27d43
jashoney <4479@holbertonschool.com>
2022-05-02 12:29:14 +1000

    added NULL test

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    new_list = []
-    length = len(my_list)
-    for i in range(length):
-        new_list.append(my_list[length - 1 - i])
-    for i in range(length):
-        print(new_list[i])
+    if my_list:
+        my_list.reverse();
+    for i in range(len(my_list)):
+        print(my_list[i])
