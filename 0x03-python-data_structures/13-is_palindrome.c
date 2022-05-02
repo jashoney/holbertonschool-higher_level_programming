413a48f867a23f7c3f53ff414374a33f138d1efa
jashoney <4479@holbertonschool.com>
2022-05-02 13:12:30 +1000

    changed task 13 logic

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,39 +10,36 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *tail = NULL, *temp = NULL, *top = NULL;
-	int length = 0, i = 0, j = 0, k = 0;
+	listint_t *temp = NULL;
+	int *top;
+	int length = 0, i = 0, j = 0;
 
 	if (head == NULL || *head == NULL)
 		return (1);
 
-	tail = *head;
-	top = *head;
-	while (tail != NULL)
+	temp = *head;
+	while (temp != NULL)
 	{
 		length++;
-		if (tail->next == NULL)
+		if (temp->next == NULL)
 			break;
-		tail = tail->next;
+		temp = temp->next;
 	}
-	length = length - 1;
+	temp = *head;
+	top = malloc(sizeof(*top) * length);
+	for (i = 0; i <= length - 1; i++)
+	{
+		top[i] = temp->n;
+		temp = temp->next;
+	}
 	i = 0;
-	j = length - i;
+	j = length - 1;
 	while (i <= j)
 	{
-		if (tail->n != top->n)
+		if (top[i] != top[j])
 			return (0);
-		k = i;
-		j--;
-		temp = top;
-		while (k < j)
-		{
-			temp = temp->next;
-			k++;
-		}
-		tail = temp;
-		top = top->next;
 		i++;
+		j--;
 	}
 	return (1);
 }
