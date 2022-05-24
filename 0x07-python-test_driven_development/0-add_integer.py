2ea1ac9d36a58f7a9a62cbbda25a02a5bdfa8e15
jashoney <4479@holbertonschool.com>
2022-05-25 08:46:04 +1000

    removed print line

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,9 +10,8 @@ def add_integer(a, b=98):
     adds a to b, raises error
     if either not int or float
     """
-    print(type(a))
 
-    if (type(a) is not int and type(a) is not float):
+    if ((type(a) is not int) and (type(a) is not float) or (a is None)):
         raise TypeError("a must be an integer")
 
     if isinstance(a, float):
