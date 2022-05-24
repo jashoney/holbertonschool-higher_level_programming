dd0bae4d10a8def0377a388718099f96e0c7454e
jashoney <4479@holbertonschool.com>
2022-05-25 07:53:23 +1000

    readding file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,55 +6,59 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """ 
-    this is a unittest for max_integer
+    this is a class unittest for the max_integer fn
     """
-
-    def list_is_empty(self):
-        """ list is empty """
-        my_list = []
-        self.assertIsNone(max_integer(my_list))
-
-
-    def list_has_one_element(self):
+    def test_list_has_one_element(self):
         """ list has only 1 element """
         my_list = [5]
         self.assertEqual(max_integer(my_list), 5)
 
-
-    def first_element_is_largest(self):
+    def test_first_element_is_largest(self):
         """ test largest as the first element """
         my_list = [99, 0, -2, 98]
-        eslf.assertEqual(max_integer(my_list, 99))
+        self.assertEqual(max_integer(my_list), 99)
 
-
-    def middle_element_is_largest(self):
+    def test_middle_element_is_largest(self):
         """ move the largest into the middle of the list """
         my_list = [0, 99, -2, 98]
-        eslf.assertEqual(max_integer(my_list, 99))
+        self.assertEqual(max_integer(my_list), 99)
 
+    def test_last_element_is_largest(self):
+        """ move the largest into the end of the list """
+        my_list = [0, -2, 99]
+        self.assertEqual(max_integer(my_list), 99)
 
-    def last_element_is_largest(self):
-        """ move the largest into the middle of the list """
-        my_list = [0, 98, -2, 99]
-        eslf.assertEqual(max_integer(my_list, 99))
-
-
-    def largest_is_a_negative_number(self):
-        """ move the largest into the middle of the list """
+    def test_largest_is_a_negative_number(self):
+        """ check for negative numbers """
         my_list = [-1, -98, -2, -99]
-        eslf.assertEqual(max_integer(my_list, -1))
+        self.assertEqual(max_integer(my_list), -1)
 
-
-    def function_doc(self):
+    def test_function_doc(self):
         """docstring for function test """
-        f = max_integer.__doc__
-        self.assertTrue(len(f) > 3)
+        doc = max_integer.__doc__
+        self.assertTrue(len(doc) > 3)
 
+    def test_module_doc(self):
+        """docstring for module test """
+        doc = __import__('6-max_integer').__doc__
+        self.assertTrue(len(doc) > 3)
 
-    def module_doc(self):
-        """docstring for module """
-        m = __import__('6-max_integer').__doc__
-        self.assertTrue(len(m) > 3)
+    def test_list_is_empty(self):
+        """ list is empty """
+        my_list = []
+        self.assertIsNone(max_integer(my_list))
+
+    def test_list_has_a_non_integer(self):
+        """ list has a string """
+        my_list = [1, 3, "hello", 5]
+        with self.assertRaises(TypeError):
+            max_integer(my_list)
+
+    def test_None(self):
+        """ use None as input """
+        list is None
+        with self.assertRaises(TypeError):
+            max_integer(None)
 
 
 if __name__ == "__main__":
