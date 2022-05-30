b30772ba27b8df4004a2e7a9be8bc7a542d1b2ee
jashoney <4479@holbertonschool.com>
2022-05-30 10:57:34 +1000

    initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    returns the list of attributes and methods for the class
+"""
+
+
+def lookup(obj):
+    """
+        lists the attributes and methods available to an object
+    """
+    return dir(obj)
