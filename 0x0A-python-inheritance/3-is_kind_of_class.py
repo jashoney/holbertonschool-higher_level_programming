246485c098aadd8d2ae89cc029bb9373b58299e0
jashoney <4479@holbertonschool.com>
2022-05-31 06:42:03 +1000

    added words to comments

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-
+    checks wether obj is an instance of a_class
 """
 
 
 def is_kind_of_class(obj, a_class):
     """
-
+        returns true if obj is an intance of a_class, or false
     """
     return isinstance(obj, a_class)
