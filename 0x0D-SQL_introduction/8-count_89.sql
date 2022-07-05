cae92c68e919a51c20d734ec6aad18523bb57a78
jashoney <4479@holbertonschool.com>
2022-07-05 18:35:52 +1000

    test

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- displays the no of records in first_table from the database hbtn_0c_0 in your MySQL server that have id=89
-SELECT COUNT (*) FROM first_table WHERE (id=89);
+SELECT COUNT(*) FROM first_table WHERE (id=89);
