66c0174a46d14c5f10dd1056b62d011b4790394a
jashoney <4479@holbertonschool.com>
2022-08-01 12:21:57 +1000

    added tasks 0, 1, 2

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// prints 3 lines to console
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// prints a msg dependant on the number of args passed
+
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
