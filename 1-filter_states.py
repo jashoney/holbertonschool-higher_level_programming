f89f11e77389a808a799e9cb1df4665e9eaec1ef
James Honey <jashoney@hotmail.com>
2022-08-04 10:30:35 +1000

    task 1

---
+++ 1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" List all state in database """
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+
+    username = argv[1]
+    upasswd = argv[2]
+    sqldb = argv[3]
+
+    conn = MySQLdb.connect(
+      host='localhost',
+      port=3306,
+      user=username,
+      passwd=upasswd,
+      db=sqldb)
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
