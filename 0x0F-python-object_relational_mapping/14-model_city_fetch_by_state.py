82a8942485465ab2362506193ebb633359fde514
James Honey <jashoney@hotmail.com>
2022-08-06 12:26:21 +1000

    changed task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -22,6 +22,6 @@ if __name__ == "__main__":
         order_by(City.id).all()
 
     for city, state in cities:
-        print(f"{state.name}: {city.id} {city.name}")
+        print(f"{state.name}: ({city.id}) {city.name}")
 
     session.close()
