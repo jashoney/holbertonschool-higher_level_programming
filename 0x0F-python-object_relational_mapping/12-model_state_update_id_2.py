c9fa91ad2a5ed50eac2df323b530b5ba6187096b
James Honey <jashoney@hotmail.com>
2022-08-11 15:28:13 +1000

    pycodestyle

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     Session.configure(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.id==2).first()
+    state = session.query(State).filter(State.id == 2).first()
     state.name = "New Mexico"
     session.add(state)
     session.commit()
