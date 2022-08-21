5bb3b4f2925d65f4574c94116f36ab733c59672a
jashoney <4479@holbertonschool.com>
2022-08-22 09:19:27 +1000

    added task 0-2

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" a Python script that fetches https://intranet.hbtn.io/status """
+
+
+if __name__ == "__main__":
+    """ our main function """
+    from urllib import request
+    url = "https://intranet.hbtn.io/status"
+    with request.urlopen(url) as response:
+        the_page = response.read()
+    print("Body response:")
+    print(f"    - type: {type(the_page)}")
+    print(f"    - content: {the_page}")
+    print(f"    - utf8 content: {the_page.decode('utf-8')}")
