197e9698b608f6ed511274d50ca74821e65322d3
jashoney <4479@holbertonschool.com>
2022-08-22 09:41:01 +1000

    fixed task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     the_page = get(url)
     the_page = the_page.text
     print("Body response:")
-    print("  - type: {}".format(type(the_page)))
-    print("  - content: {}".format(the_page))
+    print("\t- type: {}".format(type(the_page)))
+    print("\t- content: {}".format(the_page))
