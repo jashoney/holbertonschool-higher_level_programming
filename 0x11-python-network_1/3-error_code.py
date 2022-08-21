02388905c1c263bd51b5fab232f99aee2933f5ea
jashoney <4479@holbertonschool.com>
2022-08-22 09:33:37 +1000

    fixed task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
             the_page = response.read().decode("utf-8")
         print(the_page)
     except error.HTTPError as e:
-        print('Error code: ', e.code)
+        print('Error code: {}'.format(e.code))
