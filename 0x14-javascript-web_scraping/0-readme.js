a41f2f06f18ffc1ee996f10fcb61b5602381adfa
jashoney <4479@holbertonschool.com>
2022-09-13 12:43:49 +1000

    added task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file_path = process.argv[2];
+fs.readFile(file_path, 'utf-8', function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
