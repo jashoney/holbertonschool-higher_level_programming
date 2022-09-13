845c082e016154d76ed032b33c671bdb6ed1d200
jashoney <4479@holbertonschool.com>
2022-09-13 13:04:15 +1000

    added task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const axios = require('axios').default;
+const url = process.argv[2];
+
+axios.get(url)
+  .then(function (response) {
+    console.log('code: ' + response.status);
+  }).catch(function (error) {
+    console.log('code: ' + error.response.status);
+  });
